2ccf7bf7cd690329bd8e54629fd4fcea41486edd
SangYong-Jeong <mmaf@naver.com>
2021-09-03 11:21:10 +0900

    2교시 수업완료

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const app = express()
+
+require('./modules/server-init')(app, process.env.PORT)
+
+const memberRouter = require('./routes/member')
+const adminRouter = require('./routes/admin')
+const shopRouter = require('./routes/shop')
+const payRouter = require('./routes/pay')
+const mypageRouter = require('./routes/mypage')
+app.use('/member', memberRouter)
